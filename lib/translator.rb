8beb06b1245f8a64b5f0fae92a206b02f873832c
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-04-10 22:08:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,15 @@ puts emots
 
 def load_library
   emots = YAML.load_file('lib/emoticons.yml')
+  sorted_emots = {}
+  emots.each do |key, value|
+    if sorted_emots[key] == nil
+      sorted_emots[key] = {}
+    end
+    sorted_emots[key][:english] = value[0]
+    sorted_emots[key][:japanese] = value[1]
+  end
+  return sorted_emots
 end
 
 def get_japanese_emoticon
